da4db78a142d66376b2b2060d605920a8339b871
Nishi <nishi@nishi.boats>
2024-08-30 04:44:58 +0000

    adding header
    git-svn-id: file:///raid/svn-personal/irc-archiver/trunk@10 f310dea9-ad02-7e44-859a-30a050007fc7

--- Web/db.c
+++ Web/db.c
@@ -1,1 +1,7 @@
 /* $Id$ */
+
+#include "web_db.h"
+
+#include "ia_db.h"
+
+void web_db_generate(const char* name, const char* match) {}
---
+++ Web/web_db.h
@@ -0,0 +1,8 @@
+/* $Id$ */
+
+#ifndef __WEB_DB_H__
+#define __WEB_DB_H__
+
+void web_db_generate(const char* name, const char* match);
+
+#endif
